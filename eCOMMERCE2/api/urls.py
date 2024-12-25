ed10e6699c3e9703166d79cf07e5c7339f3093d5
Akram Mathu <mathuakram@gmail.com>
2024-12-25 14:10:26 +0300

    002

--- eCOMMERCE2/api/urls.py
+++ eCOMMERCE2/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet, CartViewSet, CartItemViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -10,4 +11,6 @@ router.register(r'cart-items', CartItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
